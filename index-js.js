b7ddfa73a5fe46bfb0469e309a63223bb78f9f6b
ryandd <ryandwikidermawan06@gmail.com>
2025-08-03 19:11:09 +0700

    getBalance

--- index-js.js
+++ index-js.js
@@ -1,9 +1,10 @@
-import {createWalletClient, custom, createPublicClient, parseEther, defineChain} from "https://esm.sh/viem";
+import {createWalletClient, custom, createPublicClient, parseEther, defineChain, formatEther} from "https://esm.sh/viem";
 import { contractAddress, coffeeAbi } from "./constant-js.js";
 
 const connectButton = document.getElementById('connectButton');
 const fundButton = document.getElementById('fundButton');
 const ethAmountInput = document.getElementById('ethAmount');
+const balanceButton = document.getElementById('balanceButton');
 
 let walletClient;
 let publicClient;
@@ -68,5 +69,17 @@ async function getCurrentChain(client) {
     return currentChain
 }
 
+async function getBalance() {
+    if (typeof window.ethereum !== "undefined"){
+        publicClient = createPublicClient({
+            transport: custom(window.ethereum)
+        });
+        const balance = await publicClient.getBalance({
+        address: contractAddress
+        });
+        console.log(`Contract balance: ${formatEther(balance)}`);
+    }
+}
 connectButton.onclick = connect;
 fundButton.onclick = fund;
+balanceButton.onclick = getBalance;
